a75069e77d235c9fea2026ca33bf9930346a6d2a
Dhruv Kumar <dhruvkumar@gmail.com>
2025-01-02 20:47:52 +0530

    Using Form in AddTodo

--- src/Components/AddTodo.jsx
+++ src/Components/AddTodo.jsx
@@ -13,7 +13,8 @@ function AddTodo({ onNewItem }) {
     setDueDate(event.target.value);
   };
 
-  const handleAddButtonClicked = () => {
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault();
     onNewItem(todoName, dueDate);
     setDueDate("");
     setTodoName("");
@@ -21,7 +22,7 @@ function AddTodo({ onNewItem }) {
 
   return (
     <div className="container text-center">
-      <div className="row kg-row">
+      <form className="row kg-row" onSubmit={handleAddButtonClicked}>
         <div className="col-4">
           <input
             type="text"
@@ -34,15 +35,11 @@ function AddTodo({ onNewItem }) {
           <input type="date" onChange={handleDateChange} value={dueDate} />
         </div>
         <div className="col-2">
-          <button
-            type="button"
-            className="btn btn-success kg-button"
-            onClick={handleAddButtonClicked}
-          >
+          <button type="submit" className="btn btn-success kg-button">
             <BiMessageAdd />
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
